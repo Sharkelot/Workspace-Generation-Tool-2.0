d7732eeb0e509190599348e359280cccd253d045
Sharkalot <90922996+Sharkalot@users.noreply.github.com>
2024-01-22 16:22:56 -0600

    new changes with api adjustmnets

--- wgt-backend/clickup-api-get-spaces.js
+++ wgt-backend/clickup-api-get-spaces.js
@@ -3,34 +3,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 13000;
 
 app.use(bodyParser.json());
 
-app.get('/apikeys/30967133', (req, res) => {
-  // Assuming your JSON payload looks like { "wsid": "workspace_id", "apikey": "your_api_key" }
+app.get('/apikeys/30967133', async (req, res) => {
+try {
   const { wsid, apikey } = req.body;
-  makeClickUpApiRequest(wsid, apikey)
-  .then(response => {
-    res.json(response.data);
-  })
-  .catch(error => {
-    res.status(error.response.status).json({ error: error.response.data });
-  });
+  const clickUpApiRespone = await makeClickUpApiRequest(wsid,apikey);
+  res.json(clickUpApiRespone.data);
+} catch (error) {
+  res.status(error.response.status.json)({error: error.response.data});
+}
 });
 
 async function makeClickUpApiRequest(wsid, apikey) {
   const apiUrl = `https://api.clickup.com/api/v2/space/${wsid}`;
-
-  const headers = {
+  const headers1 = {
+    method: 'GET',
+      headers: {
     'Authorization': apikey,
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json',}
   };
 
   try {
-    const response = await axios.get(apiUrl, { headers });
+    const response = await axios.get(apiUrl, { headers1 });
+    console.log ("response here:");
+    console.log(response);
     return response;
   } catch (error) {
+    console.log(error);
     throw error;
+    
   }
-}
+};
